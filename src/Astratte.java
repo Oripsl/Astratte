fbc5a3af52450dba758e7b4d848fe673fe54976f
Basile Bartolomeo <basilebartolomeo01@gmail.com>
2024-01-27 12:23:28 +0100

    first commit

---
+++ src/Astratte.java
@@ -0,0 +1,16 @@
+public class Astratte {
+
+    public static void main(String[] args) {
+        Rettangolo rettangolo = new Rettangolo(2, 5);
+
+        System.out.println(rettangolo.calcolaArea());
+
+        Triangolo triangolo = new Triangolo(5 , 9);
+
+        System.out.println(triangolo.calcolaArea());
+
+    }
+}
+
+
+
---
+++ src/Rettangolo.java
@@ -0,0 +1,14 @@
+class Rettangolo extends Forma {
+    private int altezza;
+    private int base;
+
+    public Rettangolo(int base, int altezza) {
+     this.base = base;
+     this.altezza = altezza;
+    }
+
+    @Override
+    public double calcolaArea() {
+        return base * altezza;
+    }
+}
